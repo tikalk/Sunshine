210edffdcf3acf64f61765b1bc29e041eaec4756
Shrek2000 <oren.kleks@gmail.com>
2015-12-28 22:39:49 +0200

    getType function testing

--- app/src/main/java/com/tikalk/sunshine/sunshine/data/db/WeatherProvider.java
+++ app/src/main/java/com/tikalk/sunshine/sunshine/data/db/WeatherProvider.java
@@ -152,10 +152,11 @@ public class WeatherProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
 
         switch (match) {
-            // Student: Uncomment and fill out these two cases
-//            case WEATHER_WITH_LOCATION_AND_DATE:
-//            case WEATHER_WITH_LOCATION:
-            case WEATHER:
+
+            case WEATHER_WITH_LOCATION_AND_DATE:
+                return WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE;
+            case WEATHER_WITH_LOCATION:
+               case WEATHER:
                 return WeatherContract.WeatherEntry.CONTENT_TYPE;
             case LOCATION:
                 return WeatherContract.LocationEntry.CONTENT_TYPE;
