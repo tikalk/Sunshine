aab2ede8a33c46e92a4adcc6ddc4ae642c3edd9c
Shrek2000 <oren.kleks@gmail.com>
2016-01-03 00:10:01 +0200

    row item

--- app/src/main/java/com/tikalk/sunshine/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/tikalk/sunshine/sunshine/ForecastAdapter.java
@@ -60,10 +60,10 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // our view is pretty simple here --- just a text view
-        // we'll keep the UI functional with a simple (and slow!) binding.
-
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+//        // our view is pretty simple here --- just a text view
+//        // we'll keep the UI functional with a simple (and slow!) binding.
+//
+//        TextView tv = (TextView)view;
+//        tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
