f9b5f255c3a16c1897f60f4a29df5d60d57c7022
Shrek2000 <oren.kleks@gmail.com>
2016-01-05 22:49:30 +0200

    New icons added

--- app/src/main/java/com/tikalk/sunshine/sunshine/DetailedActivityFragment.java
+++ app/src/main/java/com/tikalk/sunshine/sunshine/DetailedActivityFragment.java
@@ -151,7 +151,7 @@ public class DetailedActivityFragment extends Fragment implements LoaderManager.
             // Read weather condition ID from cursor
             int weatherId = data.getInt(COL_WEATHER_CONDITION_ID);
             // Use placeholder Image
-            mIconView.setImageResource(R.drawable.ic_launcher);
+            mIconView.setImageResource(Utility.getArtResourceForWeatherCondition(weatherId));
 
             // Read date from cursor and update views for day of week and date
             long date = data.getLong(COL_WEATHER_DATE);
